e3c073970067dd0b94b7ce0b56191d5b9815447a
coaltic <glindsay156@gmail.com>
2021-10-21 20:38:14 -0300

    [UPDATE] Wall Generation Added

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 using System;
 
 public class MapGenerator : MonoBehaviour
 {
+
     public int width;
     public int height;
 
@@ -16,13 +16,12 @@ public class MapGenerator : MonoBehaviour
 
     int[,] map;
 
-    private void Start()
+    void Start()
     {
         GenerateMap();
-            
     }
 
-    private void Update()
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
@@ -40,9 +39,28 @@ public class MapGenerator : MonoBehaviour
             SmoothMap();
         }
 
+        int borderSize = 1;
+        int[,] borderedMap = new int[width + borderSize * 2, height + borderSize * 2];
+
+        for (int x = 0; x < borderedMap.GetLength(0); x++)
+        {
+            for (int y = 0; y < borderedMap.GetLength(1); y++)
+            {
+                if (x >= borderSize && x < width + borderSize && y >= borderSize && y < height + borderSize)
+                {
+                    borderedMap[x, y] = map[x - borderSize, y - borderSize];
+                }
+                else
+                {
+                    borderedMap[x, y] = 1;
+                }
+            }
+        }
+
         MeshGenerator meshGen = GetComponent<MeshGenerator>();
-        meshGen.GenerateMesh(map, 1);
+        meshGen.GenerateMesh(borderedMap, 1);
     }
+
 
     void RandomFillMap()
     {
@@ -78,13 +96,10 @@ public class MapGenerator : MonoBehaviour
                 int neighbourWallTiles = GetSurroundingWallCount(x, y);
 
                 if (neighbourWallTiles > 4)
-                {
                     map[x, y] = 1;
-                }
                 else if (neighbourWallTiles < 4)
-                {
                     map[x, y] = 0;
-                }
+
             }
         }
     }
@@ -106,26 +121,27 @@ public class MapGenerator : MonoBehaviour
                 else
                 {
                     wallCount++;
-
                 }
             }
         }
+
         return wallCount;
     }
 
-    /*private void OnDrawGizmos()
+
+    void OnDrawGizmos()
     {
-        if (map != null)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
-                    Vector3 pos = new Vector3(-width / 2 + x + 0.5f, 0, - height / 2 + y + 0.5f);
-                    Gizmos.DrawCube(pos, Vector3.one);
+        /*
+        if (map != null) {
+            for (int x = 0; x < width; x ++) {
+                for (int y = 0; y < height; y ++) {
+                    Gizmos.color = (map[x,y] == 1)?Color.black:Color.white;
+                    Vector3 pos = new Vector3(-width/2 + x + .5f,0, -height/2 + y+.5f);
+                    Gizmos.DrawCube(pos,Vector3.one);
                 }
             }
         }
-    }*/
+        */
+    }
+
 }
